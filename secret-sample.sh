6b883df0be6ae104322de6f1a911ce9082884d1d
Frederic Stark <frederic.stark@gmail.com>
2026-04-29 11:03:24 +0200

    Added sample secret

---
+++ secret-sample.sh
@@ -0,0 +1,19 @@
+
+# Myforterro API documentation:
+# https://forterro-fwe.atlassian.net/wiki/spaces/MFT/pages/1685717006/How+to+use+the+MyForterro+API
+
+# MyForterro API Swagger
+# https://integration-myforterro-api.fcs-dev.eks.forterro.com/swagger/index.html
+# https://integration-myforterro-api.fcs-dev.eks.forterro.com/openapi/v1.json
+
+# OpenID configuration for authentication
+# https://integration-myforterro-core.fcs-dev.eks.forterro.com/.well-known/openid-configuration
+
+export CLIENT_APP_ID=
+export CLIENT_APP_SECRET=
+
+
+export TENANT_ID=
+export TENANT_APPLICATION_ID=
+
+export OPENAI_API_KEY=
